0970c40c74cf482e0d76a182f41941003c193833
MrBaduuz <joelwoerner@gmx.de>
2020-03-14 17:54:34 +0100

    Added Bombs

--- Examples/Fruit Ninja.py
+++ Examples/Fruit Ninja.py
@@ -39,10 +39,11 @@ class Fruit:
 			if list_len > 2:
 				p1 = slices[slice_ind].list[list_len-1]
 				p2 = slices[slice_ind].list[list_len-2]
-				if (p1[0]-p2[0])**2 + (p1[1]-p2[1])**2 > 10**2:
+				if (p1[0]-p2[0])**2 + (p1[1]-p2[1])**2 > 6**2:
 					global score
 					score += 1
 					self.restart()
+		return False
 
 	def restart(self):
 		self.r = random.randint(50, 70)
@@ -52,15 +53,46 @@ class Fruit:
 		self.vel = random.randint(-25, -20)
 		self.wasUp = False
 
+class Bomb:
+	def __init__(self):
+		self.r = random.randint(50, 70)
+		self.x = random.randint(self.r, eng.gameSize()[0]-self.r)
+		self.y = random.randint(eng.gameSize()[1] + 100, eng.gameSize()[1] + 190)
+		self.col = (255, 0, 0)
+		self.vel = random.randint(-25, -20)
+		self.wasUp = False
+	def show(self):
+		eng.drawEllipse(self.x, self.y, self.r, self.r, self.col)
+		eng.drawLine(self.x-10, self.y-10, self.x+10, self.y+10, (255, 255, 255, 5))
+		eng.drawLine(self.x-10, self.y+10, self.x+10, self.y-10, (255, 255, 255, 5))
+	def update(self, dt):
+		self.vel += 0.3 * dt * 120
+		self.y += self.vel * dt * 120
+		if self.y > eng.gameSize()[1] and self.wasUp:
+			global failed, menu, slices, slice_ind
+			return True
+		if self.y < eng.gameSize()[1] and not self.wasUp:
+			self.wasUp = True
+		if (mouseX-self.x)**2 + (mouseY-self.y)**2 < (self.r/2)**2 and eng.mousePressed():
+			list_len = len(slices[slice_ind].list)
+			if list_len > 2:
+				p1 = slices[slice_ind].list[list_len-1]
+				p2 = slices[slice_ind].list[list_len-2]
+				if (p1[0]-p2[0])**2 + (p1[1]-p2[1])**2 > 6**2:
+					menu = True
+		return False
+
+
 eng.createGame("Fruit Ninja", 800, 800, 120)
 eng.setIcon(sys.path[0] + "\\fruit_icon.ico")
+eng.hide_mouse(True)
 del_time = 0.1
 mouseX, mouseY = eng.mouseCoords()
 score = 0
 slices = [Slice(mouseX, mouseY)]
 slice_ind = 0
 score = 0
-f = Fruit()
+fruits = [Fruit()]
 failed = 5
 menu = True
 
@@ -72,12 +104,17 @@ def update(deltaTime):
 	global mouseX, mouseY, slices, slice_ind
 	mouseX, mouseY = eng.mouseCoords()
 	eng.drawBg(50, 50, 50)
+	eng.drawEllipse(mouseX, mouseY, 5, 5, (255, 255, 240))
 	if not menu:
+		if random.randint(1, 1200) == 1:
+			fruits.append(Bomb())
 		if eng.mousePressed():
 			slices[slice_ind].add(mouseX, mouseY)
-	
-		f.update(deltaTime)
-		f.show()
+		for fruit in fruits:
+			if fruit.update(deltaTime):
+				fruits.remove(fruit)
+				continue
+			fruit.show()
 		for slice in slices:
 			for i in range(1, len(slice.list)):
 				p1 = slice.list[i]
